27b6848e9848193cc7b8585e22bfbc0718bf46d2
Denis <69689773+Denis-Dev-2020@users.noreply.github.com>
2024-06-19 13:05:43 +0300

    Delete CryptoSurgeBackend directory

--- CryptoSurgeBackend/routes/news.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const newsController = require('../controllers/news');
-
-const router = express.Router();
-
-router.get('/', newsController.fetchAll);
-
-module.exports = router;
